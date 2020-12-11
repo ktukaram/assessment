a64bf21f250dee850cc6bda6ea03b4a33ff75735
ktukaram <72622047+ktukaram@users.noreply.github.com>
2020-12-10 22:12:18 -0600

    my code

---
+++ Array/src/array/Array.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+			//test
+		  int[] myArray = new int[10];
+		 
+		
+		for(int i=0; i<=myArray.length; i++) {
+			myArray[i] = i * 10;
+		
+		System.out.println(myArray[i]);
+		
+	}
+
+}
+}
